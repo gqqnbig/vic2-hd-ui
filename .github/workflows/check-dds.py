5c3427e9a747f849f9457e523ce281559a29baac
QQ喵 <gqqnb2005@gmail.com>
2023-06-12 17:12:51 +0800

    exit as error if there are errors during iteration

--- .github/workflows/check-dds.py
+++ .github/workflows/check-dds.py
@@ -4,10 +4,16 @@ import sys
 from PIL import Image
 
 if __name__ == '__main__':
+	hasError = False
 	for imageFile in glob.glob("./*.dds"):
 		try:
 			with Image.open(imageFile, formats=['DDS']) as im:
 				if im.format != 'DDS' or im.mode != 'RGBA':
 					print(f'{imageFile} is not in DDS 32bit-A8R8G8B8 mode.', file=sys.stderr)
+					hasError = True
 		except:
 			print(f'{imageFile} is not in DDS format.', file=sys.stderr)
+			hasError = True
+
+	if hasError:
+		exit(1)
